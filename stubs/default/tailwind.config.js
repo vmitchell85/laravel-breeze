5eb78c20698ef4abb60fc5d7cbccca41e49b1a85
Vince Mitchell <vincent.mitchell@gmail.com>
2022-07-12 14:03:31 -0400

    create brand color and default to indigo

--- stubs/default/tailwind.config.js
+++ stubs/default/tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -13,6 +14,9 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                brand: colors.indigo,
+            }
         },
     },
 
--- stubs/inertia-common/tailwind.config.js
+++ stubs/inertia-common/tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -14,6 +15,9 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                brand: colors.indigo,
+            }
         },
     },
 
